fa18e98663d6f2da6fcbf48a27265ff1b72c013c
JoeyVinyard <jviny98@yahoo.com>
2016-10-27 21:37:34 -0400

    Complete overhaul of collisions and rebounds

--- js/driver.js
+++ js/driver.js
@@ -3,8 +3,8 @@ function update(){
 	ctx.fillStyle = "White";
 	ctx.fillRect(0,0,$("#canv").attr("width"),$("#canv").attr("height"));
 	ctx.save();
-	for(var i=0;i<objects.length;i++){
-		objects[i].tick();
+	tick();
+	for(var i = 0; i < objects.length; i++){
 		drawObject(objects[i].x,objects[i].y,objects[i].width,objects[i].height,objects[i].shape,objects[i].color);
 	}
 	ctx.restore();
